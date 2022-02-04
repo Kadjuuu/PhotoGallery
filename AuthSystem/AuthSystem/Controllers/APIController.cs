3f8d0b1c075db04485cbd081efa7ca0d0a9c4afa
Kadjuuu <32106468+Kadjuuu@users.noreply.github.com>
2022-02-04 18:23:49 +0100

    API updated
    Added:
    Create, Update, Delete
    Fixed main entity request

--- AuthSystem/AuthSystem/Controllers/APIController.cs
+++ AuthSystem/AuthSystem/Controllers/APIController.cs
@@ -9,39 +9,117 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
+
+
+
+
 namespace AuthSystem.Controllers
 {
     [Route("[controller]")]
     [ApiController]
     public class APIController : ControllerBase
     {
-        private AuthDbContext _DbContext;
-        public APIController(AuthDbContext DbContext)
+        private ImageDbContext _DbContext;
+        public APIController(ImageDbContext DbContext)
         {
             _DbContext = DbContext;
 
         }
 
-        [HttpGet("GetUsers")]
+        [HttpGet("Images")]
         public IActionResult Get()
         {
             try
             {
-                var users = _DbContext.Users.ToList();
-                if (users.Count == 0)
+                var Images = _DbContext.Images.ToList();
+                if (Images.Count == 0)
                 {
                     return StatusCode(404, "No user found");
                 }
-                return Ok(users);
+                return Ok(Images);
             }
             catch (Exception)
             {
 
                 return StatusCode(500, "An error has occurred");
             }
-
         }
 
+
+        [HttpPost("CreateUser")]
+        public IActionResult Create([FromBody] ImageModel Photos)
+        {
+            ImageModel image = new ImageModel();
+            image.Title = Photos.Title;
+            image.Author = Photos.Author;
+            image.ImageFile = Photos.ImageFile;
+            image.ImageName = Photos.ImageName;
+
+            try
+            {
+                _DbContext.Images.Add(image);
+                _DbContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(500, "An error has occurred");
+            }
+            var images = _DbContext.Images.ToList();
+            return Ok(images);
+        }
+
+        [HttpPut("UpdateUser")]
+        public IActionResult Update([FromBody] ImageModel Photos)
+        {
+
+
+            try
+            {
+                var image = _DbContext.Images.FirstOrDefault(x => x.ImageId == Photos.ImageId);
+                if (image == null)
+                {
+                    return StatusCode(404, "User not found");
+                }
+                image.Title = Photos.Title;
+                image.Author = Photos.Author;
+                image.ImageFile = Photos.ImageFile;
+                image.ImageName = Photos.ImageName;
+
+                _DbContext.Entry(image).State = EntityState.Modified;
+                _DbContext.SaveChanges();
+
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(500, "An error has occurred");
+            }
+            var images = _DbContext.Images.ToList();
+            return Ok(images);
+        }
+
+        [HttpDelete("DeleteUser/{ImageId}")]
+        public IActionResult Delete([FromRoute] ImageModel Photos)
+        {
+            try
+            {
+                var image = _DbContext.Images.FirstOrDefault(x => x.ImageId == Photos.ImageId);
+                if (image == null)
+                {
+                    return StatusCode(404, "User not found");
+                }
+                _DbContext.Entry(image).State = EntityState.Deleted;
+                _DbContext.SaveChanges();
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(500, "An error has occurred");
+            }
+            var images = _DbContext.Images.ToList();
+            return Ok(images);
+        }
 
     }
 }
